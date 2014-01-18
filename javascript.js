fa1a9c98e9b43a2850912b8baaa3e0e1a485a220
David Zemon <david@stlswedespeed.com>
2014-01-18 16:37:47 -0600

    Formatting changes; Doctype -> HTML5

--- javascript.js
+++ javascript.js
@@ -1,47 +1,42 @@
-$(document).ready(function(){
-	/******************/
-	/*** Index page ***/
-	/******************/
-	// Show/hide titles
-	$("div#professional img").mouseover(function()
-	{
-		$("div#professional h2").css("color", "black");
-	});
-	$("div#personal img").mouseover(function()
-	{
-		$("div#personal h2").css("color", "black");
-	});
-	$("div .index img").mouseout(function()
-	{
-		$("div h2").css("color", "white");
-	});
-	
-	/**************/
-	/*** Resume ***/
-	/**************/
-	// Define functons
-	function toggleStuffs()
-	{
-		var id = $(this).attr("id");
-		var idContent = id + "-content";
-		if ( $("#" + idContent).css("display") == "none" )
-		{	
-			$(".resume-content").hide();
-			$("#" + idContent).show();
-		}
-		else
-			$(".resume-content").hide();
-	}
-	
-	// Show/hide sections
-	$(".resume-heading").click(toggleStuffs);
-	
-	/****************/
-	/*** PropWare ***/
-	/****************/
-    $( document ).tooltip();
-	
-	/**************************/
-	/*** Recreational pages ***/
-	/**************************/
+$(document).ready(function () {
+    /******************/
+    /*** Index page ***/
+    /******************/
+        // Show/hide titles
+    $("div#professional img").mouseover(function () {
+        $("div#professional h2").css("color", "black");
+    });
+    $("div#personal img").mouseover(function () {
+        $("div#personal h2").css("color", "black");
+    });
+    $("div .index img").mouseout(function () {
+        $("div h2").css("color", "white");
+    });
+
+    /**************/
+    /*** Resume ***/
+    /**************/
+        // Define functons
+    function toggleStuffs() {
+        var id = $(this).attr("id");
+        var idContent = id + "-content";
+        if ("none" == $("#" + idContent).css("display")) {
+            $(".resume-content").hide();
+            $("#" + idContent).show();
+        }
+        else
+            $(".resume-content").hide();
+    }
+
+    // Show/hide sections
+    $(".resume-heading").click(toggleStuffs);
+
+    /****************/
+    /*** PropWare ***/
+    /****************/
+    $(document).tooltip();
+
+    /**************************/
+    /*** Recreational pages ***/
+    /**************************/
 });
