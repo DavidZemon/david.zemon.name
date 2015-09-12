49fe8d6e3ff2ca62c3ff9d89a169ba8d70ef55ec
David Zemon <david@zemon.name>
2015-09-12 14:04:31 -0500

    Create first Maple page with content

--- src/app.js
+++ src/app.js
@@ -6,104 +6,82 @@ angular.module('davidzemon', [
         'davidzemon.resume',
         'davidzemon.cars'
     ])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .otherwise({
-                    redirectTo: '/welcome'
-                })
-        }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/welcome'})
+    }]);
 
 angular.module('davidzemon.welcome', [
         'ngRoute'
     ])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/welcome', {
-                    templateUrl: 'src/welcome/welcome.html',
-                    controller: 'WelcomeCtrl',
-                    controllerAs: 'welcome'
-                })
-        }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/welcome', {
+            templateUrl: 'src/welcome/welcome.html'
+        })
+    }]);
 
 angular.module('davidzemon.resume', [
         'ngRoute'
     ])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/resume', {
-                    templateUrl: 'src/resume/resume.html',
-                    controller: 'ResumeCtrl',
-                    controllerAs: 'resume'
-                })
-        }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/resume', {
+            templateUrl: 'src/resume/resume.html'
+        })
+    }]);
 
 angular.module('davidzemon.academia', [
         'ngRoute',
         'davidzemon.academia.gradesheet',
         'davidzemon.academia.maple'
     ])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/academia', {
-                    templateUrl: 'src/academia/academia.html',
-                    controller: 'AcademiaCtrl',
-                    controllerAs: 'academia'
-                })
-        }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/academia', {
+            templateUrl: 'src/academia/academia.html'
+        })
+    }]);
 
 angular.module('davidzemon.academia.gradesheet', [
         'ngRoute',
         'davidzemon.academia.gradesheet.changelog'
     ])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/academia/gradesheet', {
-                    templateUrl: 'src/academia/gradesheet/gradesheet.html',
-                    controller: 'GradesheetCtrl',
-                    controllerAs: 'gradesheet'
-                })
-        }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/academia/gradesheet', {
+            templateUrl: 'src/academia/gradesheet/gradesheet.html'
+        })
+    }]);
 
 angular.module('davidzemon.academia.gradesheet.changelog', [
         'ngRoute'
     ])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/academia/gradesheet/changelog', {
-                    templateUrl: 'src/academia/gradesheet/changelog/changelog.html',
-                    controller: 'ChangelogCtrl',
-                    controllerAs: 'changelog'
-                })
-        }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/academia/gradesheet/changelog', {
+            templateUrl: 'src/academia/gradesheet/changelog/changelog.html'
+        })
+    }]);
 
 angular.module('davidzemon.academia.maple', [
+        'ngRoute',
+        'davidzemon.academia.maple.gettingStarted'
+    ])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/academia/maple', {
+            templateUrl: 'src/academia/maple/maple.html'
+        })
+    }]);
+
+angular.module('davidzemon.academia.maple.gettingStarted', [
         'ngRoute'
     ])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/academia/maple', {
-                    templateUrl: 'src/academia/maple/maple.html',
-                    controller: 'MapleCtrl',
-                    controllerAs: 'maple'
-                })
-        }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/academia/maple/gettingStarted', {
+            templateUrl: 'src/academia/maple/gettingStarted/gettingStarted.html'
+        })
+    }]);
 
 angular.module('davidzemon.cars', [
         'ngRoute'
     ])
-    .config(['$routeProvider',
-        function ($routeProvider) {
-            $routeProvider
-                .when('/cars', {
-                    templateUrl: 'src/cars/cars.html',
-                    controller: 'CarsCtrl',
-                    controllerAs: 'cars'
-                })
-        }]);
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/cars', {
+            templateUrl: 'src/cars/cars.html'
+        })
+    }]);
